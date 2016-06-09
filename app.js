6f8e747e6f4c54ddb7d21650bb1ad7f4cc2bb507
BerniWittmann <wittmann_b@web.de>
2016-06-09 16:58:23 -0700

    Test fix

--- app.js
+++ app.js
@@ -14,12 +14,12 @@ var mongoose = require('mongoose');
 var passport = require('passport');
 
 // connect MongoDB
-mongoose.connect(app.get('MONGODB_URI'), function(err,db){
-    if (!err){
-        console.log('Connected to mLab');
-    } else{
-        console.log(err); //failed to connect
-    }
+mongoose.connect(app.get('MONGODB_URI'), function (err, db) {
+	if (!err) {
+		console.log('Connected to mLab');
+	} else {
+		console.log(err); //failed to connect
+	}
 });
 
 require('./models/Gruppen');
@@ -28,64 +28,6 @@ require('./models/Spiele');
 require('./models/Spielplan');
 require('./models/Teams');
 //require('./models/Users');
-require('./config/passport');
-
-var routes = require('./routes/index');
-var users = require('./routes/users');
-
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
-
-app.use(favicon(__dirname + '/public/assets/img/hoelle_sued_logo_klein.png'));
-app.use(logger('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-app.use(passport.initialize());
-
-app.use('/', routes);
-app.use('/users', users);
-
-
-app.set('port', (process.env.PORT || 8000));
-
-app.listen(app.get('port'), function() {
-  console.log('Node app is running on port', app.get('port'));
-});
-
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-    var err = new Error('Not Found');
-    err.status = 404;
-    next(err);
-});
-
-// error handlers
-
-// development error handler
-// will print stacktrace
-if (app.get('env') === 'development') {
-    app.use(function(err, req, res, next) {
-        res.status(err.status || 500);
-        res.render('error', {
-            message: err.message,
-            error: err
-        });
-    });
-}
-
-// production error handler
-// no stacktraces leaked to user
-app.use(function(err, req, res, next) {
-    res.status(err.status || 500);
-    res.render('error', {
-        message: err.message,
-        error: {}
-    });
-});
-
 var crypto = require('crypto');
 var jwt = require('jsonwebtoken');
 
@@ -127,8 +69,67 @@ UserSchema.methods.validPassword = function (password) {
 };
 
 mongoose.model('User', UserSchema);
+require('./config/passport');
+
+var routes = require('./routes/index');
+var users = require('./routes/users');
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+app.use(favicon(__dirname + '/public/assets/img/hoelle_sued_logo_klein.png'));
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+	extended: false
+}));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(passport.initialize());
+
+app.use('/', routes);
+app.use('/users', users);
+
+
+app.set('port', (process.env.PORT || 8000));
+
+app.listen(app.get('port'), function () {
+	console.log('Node app is running on port', app.get('port'));
+});
+
+// catch 404 and forward to error handler
+app.use(function (req, res, next) {
+	var err = new Error('Not Found');
+	err.status = 404;
+	next(err);
+});
+
+// error handlers
+
+// development error handler
+// will print stacktrace
+if (app.get('env') === 'development') {
+	app.use(function (err, req, res, next) {
+		res.status(err.status || 500);
+		res.render('error', {
+			message: err.message
+			, error: err
+		});
+	});
+}
+
+// production error handler
+// no stacktraces leaked to user
+app.use(function (err, req, res, next) {
+	res.status(err.status || 500);
+	res.render('error', {
+		message: err.message
+		, error: {}
+	});
+});
+
+
 
 
 module.exports = app;
-
-
