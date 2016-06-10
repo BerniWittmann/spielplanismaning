a057155df6c1a3ea0931ad6d1fae47a2251260a6
BerniWittmann <wittmann_b@web.de>
2016-06-09 17:04:49 -0700

    Test

--- app.js
+++ app.js
@@ -27,48 +27,7 @@ require('./models/Jugenden');
 require('./models/Spiele');
 require('./models/Spielplan');
 require('./models/Teams');
-//require('./models/Users');
-var crypto = require('crypto');
-var jwt = require('jsonwebtoken');
-
-var UserSchema = new mongoose.Schema({
-	username: {
-		type: String
-		, lowercase: true
-		, unique: true
-	}
-	, hash: String
-	, salt: String
-});
-
-console.log('######' + process.env.SECRET + '####');
-UserSchema.methods.generateJWT = function () {
-
-	// set expiration to 60 days
-	var today = new Date();
-	var exp = new Date(today);
-	exp.setDate(today.getDate() + 60);
-
-	return jwt.sign({
-		_id: this._id
-		, username: this.username
-		, exp: parseInt(exp.getTime() / 1000)
-	, }, 'SECRET');
-};
-
-UserSchema.methods.setPassword = function (password) {
-	this.salt = crypto.randomBytes(16).toString('hex');
-
-	this.hash = crypto.pbkdf2Sync(password, this.salt, 1000, 64).toString('hex');
-};
-
-UserSchema.methods.validPassword = function (password) {
-	var hash = crypto.pbkdf2Sync(password, this.salt, 1000, 64).toString('hex');
-
-	return this.hash === hash;
-};
-
-mongoose.model('User', UserSchema);
+require('./models/Users')('TEst');
 require('./config/passport');
 
 var routes = require('./routes/index');
