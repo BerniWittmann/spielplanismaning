90d19c20d89659c90bd375a7a9d331e80c3314b4
BerniWittmann <wittmann_b@web.de>
2016-06-11 11:44:43 -0700

    Main Loader bei State change

--- public/app.js
+++ public/app.js
@@ -5,7 +5,8 @@
 		.module('spi', [
             'spi.auth', 'spi.logger', 'spi.home.ui', 'spi.login.ui', 'spi.tgj.ui', 'spi.navigation.ui', 'spi.team', 'spi.verwaltung.ui', 'ui.router', 'spi.tabellen.ui', 'spi.login.ui', 'spi.spielplan.ui', 'spi.jugenden.jugendlabel.ui', 'spi.spiel.ui', 'spi.tabelle.ui', 'spi.footer.ui', 'spi.loader.ui'
         ])
-		.config(states);
+		.config(states)
+		.run(run);
 
 	function states($urlRouterProvider, $stateProvider) {
 		$urlRouterProvider.otherwise('/home');
@@ -16,5 +17,16 @@
 				, template: '<ui-view></ui-view>'
 				, abstract: true
 			});
+	}
+
+	function run($state, $rootScope) {
+		$rootScope.$on('$stateChangeStart', function () {
+			$rootScope.loading = true;
+		});
+
+
+		$rootScope.$on('$stateChangeSuccess', function () {
+			$rootScope.loading = false;
+		});
 	}
 })();
