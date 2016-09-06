2f7bf0cb879792f7d7fe09d31d3de348fe2a6b98
BerniWittmann <wittmann_b@web.de>
2016-09-06 19:48:09 +0200

    Revert "Be Test Anpassung Testserver Body parser"
    This reverts commit 760af69eee8366ed08c7bf4ad913edc668c08fc9.

--- test/backend/testserver.js
+++ test/backend/testserver.js
@@ -2,6 +2,7 @@ module.exports = function (env) {
     var express = require('express');
     var app = express();
     var mongoose = require('mongoose');
+    var bodyParser = require('body-parser');
 
     process.env.SECRET = process.env.SECRET || env.SECRET || 'TEST-SECRET';
     process.env.ENVIRONMENT = process.env.ENVIRONMENT || env.ENVIRONMENT || 'TEST';
@@ -45,6 +46,9 @@ module.exports = function (env) {
     var users = require('../../routes/users')();
     var email = require('../../routes/email')(sendgrid, (process.env.ENVIRONMENT || 'DEV'), (process.env.URL || 'http://localhost:8000/'));
     var config = require('../../routes/config')(process.env);
+
+    app.use(bodyParser.json());
+    app.use(bodyParser.urlencoded({extended: false}));
 
     app.use('/api', routes);
     app.use('/api/users', users);
