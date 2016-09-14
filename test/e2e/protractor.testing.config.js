86b65c077dc5d2556d98c71f6c4296f44e4a23e2
BerniWittmann <wittmann_b@web.de>
2016-09-14 17:43:21 +0200

    E2E Tests Travis Sauce Connect
    capabilites

--- test/e2e/protractor.testing.config.js
+++ test/e2e/protractor.testing.config.js
@@ -2,12 +2,15 @@ exports.config = {
     framework: 'jasmine',
     seleniumAddress: 'ondemand.saucelabs.com:80',
     specs: ['*.spec.js'],
-    capabilities: {
-        'browserName': 'firefox'
-    },
     sauceUser: process.env.SAUCE_USERNAME,
     sauceKey: process.env.SAUCE_ACCESS_KEY,
     sauceSeleniumAddress: 'ondemand.saucelabs.com:80/wd/hub',
+    capabilities: {
+        'browserName': 'firefox',
+        'tunnel-identifier': process.env.TRAVIS_JOB_NUMBER,
+        'build': process.env.TRAVIS_BUILD_NUMBER,
+        'name': 'Travis Test on Testing'
+    },
     onPrepare: function () {
         console.log(process.env.SAUCE_USERNAME);
         console.log(process.env.SAUCE_ACCESS_KEY);
