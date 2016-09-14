928d7f4b1ad9542ab5e564dafec59e2cb1d62091
BerniWittmann <wittmann_b@web.de>
2016-09-14 17:32:34 +0200

    E2E Tests Travis Sauce Connect
    sauceUser

--- test/e2e/protractor.testing.config.js
+++ test/e2e/protractor.testing.config.js
@@ -5,6 +5,9 @@ exports.config = {
     capabilities: {
         'browserName': 'firefox'
     },
+    sauceUser: 'BerniWittmann',
+    sauceKey: 'd0e5251a-18e3-49d9-b478-6136db1244f3',
+    sauceSeleniumAddress: 'ondemand.saucelabs.com:80/wd/hub',
     onPrepare: function () {
         var SpecReporter = require('jasmine-spec-reporter');
         // add jasmine spec reporter
