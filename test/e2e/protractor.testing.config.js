6515f06ddc14ba6c39958c6d091982ad6395affd
BerniWittmann <wittmann_b@web.de>
2016-09-14 17:40:03 +0200

    E2E Tests Travis Sauce Connect
    sauceUser

--- test/e2e/protractor.testing.config.js
+++ test/e2e/protractor.testing.config.js
@@ -5,19 +5,21 @@ exports.config = {
     capabilities: {
         'browserName': 'firefox'
     },
-    sauceUser: 'BerniWittmann',
-    sauceKey: 'd0e5251a-18e3-49d9-b478-6136db1244f3',
+    sauceUser: process.env.SAUCE_USERNAME,
+    sauceKey: process.env.SAUCE_ACCESS_KEY,
     sauceSeleniumAddress: 'ondemand.saucelabs.com:80/wd/hub',
     onPrepare: function () {
+        console.log(process.env.SAUCE_USERNAME);
+        console.log(process.env.SAUCE_ACCESS_KEY);
         var SpecReporter = require('jasmine-spec-reporter');
         // add jasmine spec reporter
         jasmine.getEnv().addReporter(new SpecReporter({displayStacktrace: 'all'}));
 
-        browser.get('www.spielplanismaning-testing.herokuapp.com');
+        browser.get('http://www.spielplanismaning-testing.herokuapp.com');
     },
     jasmineNodeOpts: {
         print: function () {
         }
     },
-    baseUrl: 'www.spielplanismaning-testing.herokuapp.com'
+    baseUrl: 'http://www.spielplanismaning-testing.herokuapp.com'
 };
