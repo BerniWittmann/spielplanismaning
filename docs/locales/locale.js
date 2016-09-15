339df605a8b5294518be5ce3c97c466b03adf3df
BerniWittmann <wittmann_b@web.de>
2016-09-15 11:21:18 +0200

    Api DOc update

--- docs/locales/locale.js
+++ docs/locales/locale.js
@@ -10,7 +10,7 @@ define([
     './locales/ru.js',
     './locales/zh.js',
     './locales/zh_cn.js'
-], function () {
+], function() {
     var langId = (navigator.language || navigator.userLanguage).toLowerCase().replace('-', '_');
     var language = langId.substr(0, 2);
     var locales = {};
@@ -19,10 +19,10 @@ define([
         for (property in arguments[index])
             locales[property] = arguments[index][property];
     }
-    if (!locales['en'])
+    if ( ! locales['en'])
         locales['en'] = {};
 
-    if (!locales[langId] && !locales[language])
+    if ( ! locales[langId] && ! locales[language])
         language = 'en';
 
     var locale = (locales[langId] ? locales[langId] : locales[language]);
@@ -39,9 +39,9 @@ define([
     }
 
     return {
-        __: __,
-        locales: locales,
-        locale: locale,
+        __         : __,
+        locales    : locales,
+        locale     : locale,
         setLanguage: setLanguage
     };
 });
