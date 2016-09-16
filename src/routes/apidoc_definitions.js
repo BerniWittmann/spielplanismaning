2039c981c26bd8c09b5f06aa4eb18022838a3f9a
BerniWittmann <wittmann_b@web.de>
2016-09-16 17:12:02 +0200

    API Doc - Config

---
+++ src/routes/apidoc_definitions.js
@@ -0,0 +1,14 @@
+/**
+ * @apiDefine TeamObject
+ * @apiSuccess {Object} team Team-Object
+ */
+
+/**
+ * @apiDefine GruppeObject
+ * @apiSuccess {Object} gruppe Gruppe-Object
+ */
+
+/**
+ * @apiDefine JugendObject
+ * @apiSuccess {Object} jugend Jugend-Object
+ */
