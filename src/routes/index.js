cb240b515afd2ac105bcfa42f68396fd3f1e08d5
BerniWittmann <wittmann_b@web.de>
2016-09-17 12:14:06 +0200

    API Doc - Jugenden
    + index doc entfernt

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,15 +2,6 @@ module.exports = function () {
     var express = require('express');
     var router = express.Router();
 
-    /**
-     * @api {get} / Render Page
-     * @apiName RenderPage
-     * @apiDescription Lädt die Seite
-     * @apiGroup index
-     *
-     *
-     * @apiSuccess {html} body HTML-Struktur der Seite
-     **/
     router.get('/', function (req, res) {
         return res.render('index');
     });
