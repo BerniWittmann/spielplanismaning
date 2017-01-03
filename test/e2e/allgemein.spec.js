8ab0abdb1a42084adc18c0305ec4be6619a647a3
BerniWittmann <wittmann_b@web.de>
2017-01-03 13:20:08 +0100

    added done callbacks to e2e tests

--- test/e2e/allgemein.spec.js
+++ test/e2e/allgemein.spec.js
@@ -1,24 +1,27 @@
 describe('Spielplan-Ismaning App', function () {
     var utils = require('./utils.js')();
 
-    beforeAll(function () {
+    beforeAll(function (done) {
         browser.get(utils.getBaseURI());
+        return done();
     });
 
-    it('soll einen Titel haben', function () {
+    it('soll einen Titel haben', function (done) {
         expect(browser.getTitle()).toEqual('Spielplan');
     });
 
-    it('soll die Navigation laden', function () {
+    it('soll die Navigation laden', function (done) {
         var result = element(by.css('spi-navigation'));
         expect(result).toBeDefined();
         expect(result.isDisplayed()).toBeTruthy();
+        return done();
     });
 
-    it('soll die Page laden', function () {
-        var result = element(by.id('page'))
+    it('soll die Page laden', function (done) {
+        var result = element(by.id('page'));
         expect(result).toBeDefined();
         expect(result.isDisplayed()).toBeTruthy();
         expect(result.getCssValue('opacity')).not.toBe(0);
+        return done();
     });
 });
--- test/e2e/navigation.spec.js
+++ test/e2e/navigation.spec.js
@@ -2,62 +2,72 @@ describe('Navigation', function () {
     var utils = require('./utils.js')();
     var navigation = require('./PageObjects/navigation.js')();
 
-    beforeAll(function () {
+    beforeAll(function (done) {
         browser.get(utils.getBaseURI());
+        return done();
     });
 
-    it('Standardmäßig soll die Home-Seite geladen werden', function () {
+    it('Standardmäßig soll die Home-Seite geladen werden', function (done) {
         expect(element(by.css('.page-header h3')).isDisplayed()).toBeTruthy();
         expect(utils.getPageHeaderText()).toEqual('Kinderbeachturnier');
         expect(browser.getCurrentUrl()).toEqual(utils.getBaseURI() + '/home');
+        return done();
     });
 
-    it('Man soll zur Spielplan-Seite navigieren können', function () {
+    it('Man soll zur Spielplan-Seite navigieren können', function (done) {
         navigation.klickeNavElement('spielplan');
         expect(utils.getPageHeaderText()).toEqual('Spielplan');
         expect(browser.getCurrentUrl()).toEqual(utils.getBaseURI() + '/spielplan');
+        return done();
     });
 
-    it('Man soll zur Tabellen-Seite navigieren können', function () {
+    it('Man soll zur Tabellen-Seite navigieren können', function (done) {
         navigation.klickeNavElement('tabellen');
         expect(utils.getPageHeaderText()).toEqual('Tabellen');
         expect(browser.getCurrentUrl()).toEqual(utils.getBaseURI() + '/tabellen');
+        return done();
     });
 
-    it('Man soll zur Home-Seite navigieren können', function () {
+    it('Man soll zur Home-Seite navigieren können', function (done) {
         navigation.klickeNavElement('home');
         expect(utils.getPageHeaderText()).toEqual('Kinderbeachturnier');
         expect(browser.getCurrentUrl()).toEqual(utils.getBaseURI() + '/home');
+        return done();
     });
 
     describe('Teeams, Gruppen & Jugenden Dropdown', function () {
-        beforeEach(function () {
+        beforeEach(function (done) {
             navigation.klickeNavElement('tgj');
             expect(navigation.isNavDropdownVisible).toBeTruthy();
+            return done();
         });
 
-        it('Man soll zur Teams-Seite navigieren können', function () {
+        it('Man soll zur Teams-Seite navigieren können', function (done) {
             navigation.klickeNavElement('tgj-teams');
             expect(utils.getPageHeaderText()).toEqual('Teams');
             expect(browser.getCurrentUrl()).toEqual(utils.getBaseURI() + '/teams');
+            return done();
         });
 
-        it('Man soll zur Gruppen-Seite navigieren können', function () {
+        it('Man soll zur Gruppen-Seite navigieren können', function (done) {
             navigation.klickeNavElement('tgj-gruppen');
             expect(utils.getPageHeaderText()).toEqual('Gruppen');
             expect(browser.getCurrentUrl()).toEqual(utils.getBaseURI() + '/gruppen');
+            return done();
         });
 
-        it('Man soll zur Jugenden-Seite navigieren können', function () {
+        it('Man soll zur Jugenden-Seite navigieren können', function (done) {
             navigation.klickeNavElement('tgj-jugenden');
             expect(utils.getPageHeaderText()).toEqual('Jugenden');
             expect(browser.getCurrentUrl()).toEqual(utils.getBaseURI() + '/jugenden');
+            return done();
         });
     });
 
-    it('Man soll zur Login-Seite navigieren können', function () {
+    it('Man soll zur Login-Seite navigieren können', function (done) {
         navigation.klickeNavElement('login');
         expect(element(by.tagName('h3')).getText()).toEqual('Login');
         expect(browser.getCurrentUrl()).toEqual(utils.getBaseURI() + '/login');
+        return done();
     });
 });
