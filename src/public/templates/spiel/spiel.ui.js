cd1be5eae767dbe53d18144cb5a676b88271027a
BerniWittmann <wittmann_b@web.de>
2017-02-10 14:03:46 +0100

    added flipclock

--- src/public/templates/spiel/spiel.ui.js
+++ src/public/templates/spiel/spiel.ui.js
@@ -6,7 +6,8 @@
             'ui.router', 'spi.spiel'
         ])
         .config(states)
-        .controller('SpielController', SpielController);
+        .controller('SpielController', SpielController)
+        .directive('flipcounter', flipcounter);
 
     function states($stateProvider) {
         $stateProvider
@@ -22,6 +23,38 @@
                 }
             });
 
+    }
+
+    function flipcounter() {
+        return {
+            restrict: 'A',
+            link: function (scope, element, attrs) {
+                var options = {
+                    clockFace: 'MinuteCounter',
+                    autoPlay: false
+                };
+                _.extend(options, scope.$eval(attrs.flipcounter));
+                var scoreA = scope.$eval(attrs.scorea);
+                var scoreB = scope.$eval(attrs.scoreb);
+                var time = 60 * scoreA + scoreB;
+
+                var start = time - scoreB - 1;
+                if (start < 0) {
+                    start = 0;
+                }
+                var clock = $(element).FlipClock(start, options);
+                clock.stop();
+                if (scoreA > 0) {
+                    clock.flip();
+                }
+                if (time > 0 && scoreB > 0) {
+                    clock.setTime(time - 1);
+                }
+                if (scoreB > 0) {
+                    clock.flip();
+                }
+            }
+        }
     }
 
     function SpielController($state, aktivesSpiel) {
