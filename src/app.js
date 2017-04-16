92f387cf42e05771178eff6e4655dd0a92ab26b3
BerniWittmann <wittmann_b@web.de>
2017-04-16 22:19:14 +0200

    rewrite error handling

--- src/app.js
+++ src/app.js
@@ -127,28 +127,12 @@ app.use(function (req, res, next) {
 });
 
 // error handlers
-
-// development error handler
-// will print stacktrace
-if (app.get('ENVIRONMENT') === 'development') {
-    app.use(function (err, req, res) {
-        appLogger.error('Error ' + err.status || 500 + ' ' + err.name, {error: err});
-        res.status(err.status || 500);
-        res.render('error', {
-            message: err.message
-            , error: err
-        });
-    });
-}
-
-// production error handler
-// no stacktraces leaked to user
 app.use(function (err, req, res) {
     appLogger.error('Error ' + err.status || 500 + ' ' + err.name, {error: err});
     res.status(err.status || 500);
     res.render('error', {
-        message: err.message
-        , error: {}
+        message: err.message,
+        err: err
     });
 });
 
