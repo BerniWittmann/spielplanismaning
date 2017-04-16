be0996a21eb7a6f48f27dbde961ac3d5ade0aa84
BerniWittmann <wittmann_b@web.de>
2017-04-16 21:52:42 +0200

    use forever on production for starting server

--- gulp-tasks/serve.js
+++ gulp-tasks/serve.js
@@ -4,23 +4,48 @@ const nodemon = require('gulp-nodemon');
 let notify = require('gulp-notify');
 let livereload = require('gulp-livereload');
 const open = require('gulp-open');
+const forever = require('forever-monitor');
 // serve
 gulp.task('serve', function (done) {
     runSequence('build', 'start:server', done);
 });
 
 gulp.task('serve:dist', function (done) {
-    runSequence('build:dist', 'start:server', done);
+    runSequence('build:dist', 'start:server:prod', done);
 });
 
 gulp.task('start:server', function (done) {
-    nodemon({
+    const stream = nodemon({
         // the script to run the app
         script: './dist/app.js',
         ignore: ['./../src', './../test']
-    }).on('end', function () {
+    });
+
+    stream.on('end', function () {
         done();
     });
+
+    stream.on('crash', function () {
+        console.error('Application has crashed!\n');
+    });
+});
+
+gulp.task('start:server:prod', function () {
+    const monitor = new forever.Monitor('./dist/app.js', {
+        max: 10,
+        minUptime: 3000,
+        spinSleepTime: 5000
+    });
+
+    monitor.on('restart', function() {
+        console.error('Forever restarting script for ' + monitor.times + ' time');
+    });
+
+    monitor.on('exit:code', function(code) {
+        console.error('Forever detected script exited with code ' + code);
+    });
+
+    monitor.start();
 });
 
 gulp.task('quit:server', function () {
