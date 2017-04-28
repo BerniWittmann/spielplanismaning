56b4a197ddae0687ffecde46836a3e12fb676577
BerniWittmann <wittmann_b@web.de>
2017-04-28 15:12:53 +0200

    fixed Access-Control Header

--- src/routes/middleware/allowCrossDomain.js
+++ src/routes/middleware/allowCrossDomain.js
@@ -2,7 +2,19 @@ module.exports = function (req, res, next) {
     const _ = require('lodash');
 
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-    res.setHeader('Access-Control-Allow-Origin', process.env.ALLOWED_ORIGINS);
+    if (process.env.ALLOWED_ORIGINS === '*') {
+        res.setHeader('Access-Control-Allow-Origin', '*');
+        return next();
+    }
+    const allowedOrigins = process.env.ALLOWED_ORIGINS.split(',').map(function (single) {
+        single = single.trim();
+        return single.replace(/(^\w+:|^)\/\//, '');
+    });
 
-    return next();
+    if (_.includes(allowedOrigins, req.get('Host'))) {
+        const origin = req.get('Origin') || (req.secure ? 'https://' : 'http://') + req.get('Host');
+        res.setHeader('Access-Control-Allow-Origin', origin);
+    }
+
+    next();
 };
