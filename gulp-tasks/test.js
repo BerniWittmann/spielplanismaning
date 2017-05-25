a4897c5228efb5dd97d4d737199d771910f1f6a0
BerniWittmann <wittmann_b@web.de>
2017-05-25 14:37:05 +0200

    rewrote backend tests: jugenden

--- gulp-tasks/test.js
+++ gulp-tasks/test.js
@@ -51,7 +51,7 @@ gulp.task('test:frontend:watch', function (done) {
 
 // test backend
 gulp.task('test:backend', function (done) {
-    return gulp.src('././test/backend/**/jugenden.spec.js', {read: false})
+    gulp.src('././test/backend/**/jugenden.spec.js', {read: false})
         .pipe(mocha({
             reporter: 'mochawesome',
             reporterOptions: {
